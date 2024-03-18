ac7a0b57da705087d0bf5e61e985dc51e91cefd9
Robena Rasolond <82022919+Robe-Ras@users.noreply.github.com>
2024-03-18 21:58:45 +0100

    model order

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_18_192819) do
     t.string "firstname"
     t.string "lastname"
     t.string "email"
+    t.boolean "creator", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "encrypted_password", default: "", null: false
@@ -58,6 +59,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_18_192819) do
     t.integer "price"
     t.datetime "start_date"
     t.integer "duration"
+    t.boolean "event", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
