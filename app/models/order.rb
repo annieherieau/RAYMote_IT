ee5a24bc51cb796f3d29390c10ca321d7d0baf37
Robena Rasolond <82022919+Robe-Ras@users.noreply.github.com>
2024-03-18 22:17:52 +0100

    model order1

--- app/models/order.rb
+++ app/models/order.rb
@@ -15,10 +15,6 @@ class Order < ApplicationRecord
   before_save :amount
 
 
-  # After create
-  # after_create :send_alert_to_admin
-  # after_create :send_alert_to_user
-
 
   # Methods
   def amount
@@ -34,20 +30,5 @@ class Order < ApplicationRecord
       order_workshops.create(workshop_id: self.id, workshop_id: workshop_id)
     end
   end
-
-
-  # private
-
-  # def send_alert_to_admin
-  #   if order.present? 
-  #     UserMailer.order_alert_admin(admin).deliver_now
-  #   end
-  # end
-
-  # def send_alert_to_user
-  #   if order.present?
-  #     UserMailer.order_alert_user(user).deliver_now
-  #   end
-  # end
 
 end
