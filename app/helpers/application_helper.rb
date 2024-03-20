33d3f2acafa5427be5207e81e46f194209524bf8
annieherieau <annie.herieau@gmail.com>
2024-03-20 17:25:05 +0100

    intégration du template

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  # formate le nombre en Euros
+  def number_to_euros(number)
+    return number_to_currency(number, unit: "€", separator: ",", format: "%n %u")
+  end
 end
