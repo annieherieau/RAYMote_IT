15ad08b4cc7c16bff9e451ba567c5f7a9b314aff
annieherieau <annie.herieau@gmail.com>
2024-03-21 16:51:21 +0100

    devise ressource_name en Fr

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
   def number_to_euros(number)
     return number_to_currency(number, unit: "€", separator: ",", format: "%n %u")
   end
+
+  # Devise en français
+  def devise_fr
+    resource_name.to_s.humanize == 'User' ? 'Utilisateur' : 'Admin'
+  end
 end
