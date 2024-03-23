38d4f961dd6874a2b3ebb487ac8e8ca163ccc343
annieherieau <annie.herieau@gmail.com>
2024-03-23 20:07:53 +0100

    lien dashboard footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,9 @@ class ApplicationController < ActionController::Base
         redirect_back(fallback_location: root_path)
       end
     end
+
+    def not_found!
+      render file: "#{Rails.root}/public/404.html", layout: false, status: :not_found
+    end
   end
   
