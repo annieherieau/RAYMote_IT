87072e42383a1ada124efaa3a0dbacf627ca0bed
annieherieau <annie.herieau@gmail.com>
2024-03-25 13:48:45 +0100

    avatar ok

--- app/javascript/tags.js
+++ app/javascript/tags.js
@@ -5,49 +5,56 @@ document.addEventListener("DOMContentLoaded", function() {
     const tagList = document.getElementById('tag-list');
   
     // Ajout de nouveaux tags
-    tagInput.addEventListener('keypress', function(e) {
-      if (e.key === 'Enter' && tagInput.value.trim() !== '') {
-        e.preventDefault();
-        const tagName = tagInput.value.trim();
-        newTags.push(tagName); // Stocke le nouveau tag
-  
-        // Crée et ajoute le badge Bootstrap pour le nouveau tag
-        const newTag = document.createElement('span');
-        newTag.textContent = tagName;
-        newTag.classList.add('badge', 'bg-primary');
-        tagList.appendChild(newTag);
-  
-        tagInput.value = ''; // Réinitialise le champ d'entrée
-      }
-    });
+    if (tagInput) {
+      tagInput.addEventListener('keypress', function(e) {
+        if (e.key === 'Enter' && tagInput.value.trim() !== '') {
+          e.preventDefault();
+          const tagName = tagInput.value.trim();
+          newTags.push(tagName); // Stocke le nouveau tag
+    
+          // Crée et ajoute le badge Bootstrap pour le nouveau tag
+          const newTag = document.createElement('span');
+          newTag.textContent = tagName;
+          newTag.classList.add('badge', 'bg-primary');
+          tagList.appendChild(newTag);
+    
+          tagInput.value = ''; // Réinitialise le champ d'entrée
+        }
+      });
+    }
+    
   
     // Soumission du formulaire
-    form.addEventListener('submit', function(e) {
-      newTags.forEach(tagName => {
-        const hiddenInput = document.createElement('input');
-        hiddenInput.type = 'hidden';
-        hiddenInput.name = 'workshop[tag_names][]';
-        hiddenInput.value = tagName;
-        form.appendChild(hiddenInput);
+    if (form) {
+      form.addEventListener('submit', function(e) {
+        newTags.forEach(tagName => {
+          const hiddenInput = document.createElement('input');
+          hiddenInput.type = 'hidden';
+          hiddenInput.name = 'workshop[tag_names][]';
+          hiddenInput.value = tagName;
+          form.appendChild(hiddenInput);
+        });
       });
-    });
+    }
   
     // Gestion des clics pour la suppression des tags
-    tagList.addEventListener('click', function(e) {
-      if (e.target.classList.contains('remove-tag')) {
-        e.preventDefault();
-        const tagElement = e.target.closest('.destroyable');
-        const tagId = e.target.getAttribute('data-tag-id');
-  
-        // Crée un champ caché pour marquer le tag comme supprimé
-        const hiddenInputForDeletion = document.createElement('input');
-        hiddenInputForDeletion.type = 'hidden';
-        hiddenInputForDeletion.name = 'workshop[deleted_tag_ids][]';
-        hiddenInputForDeletion.value = tagId;
-        form.appendChild(hiddenInputForDeletion);
-  
-        tagElement.remove(); // Supprime visuellement l'étiquette du tag
-      }
-    });
+    if (tagList) {
+      tagList.addEventListener('click', function(e) {
+        if (e.target.classList.contains('remove-tag')) {
+          e.preventDefault();
+          const tagElement = e.target.closest('.destroyable');
+          const tagId = e.target.getAttribute('data-tag-id');
+    
+          // Crée un champ caché pour marquer le tag comme supprimé
+          const hiddenInputForDeletion = document.createElement('input');
+          hiddenInputForDeletion.type = 'hidden';
+          hiddenInputForDeletion.name = 'workshop[deleted_tag_ids][]';
+          hiddenInputForDeletion.value = tagId;
+          form.appendChild(hiddenInputForDeletion);
+    
+          tagElement.remove(); // Supprime visuellement l'étiquette du tag
+        }
+      });
+    }
   });
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'avatars/create'
   resources :messages
  
   resources :reviews
