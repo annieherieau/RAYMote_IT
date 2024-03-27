cd96f86094a14624b1170b52c44200843251709a
Ton nom d'utilisateur githubYannRZG <yann.rezigui@outlook.fr>
2024-03-27 16:22:31 +0100

    community_stats

--- app/controllers/dynamic_pages_controller.rb
+++ app/controllers/dynamic_pages_controller.rb
@@ -2,9 +2,25 @@ class DynamicPagesController < ApplicationController
 
   # Page d'accueil
   def home
+    #Créateurs d'ateliers
+    @creator_users = User.joins(:workshops).distinct    
+    @total_creator_users = @creator_users.count
+
+    #Etudiants non créateurs
+    @non_creator_users = User.where.not(id: Workshop.pluck(:creator_id).uniq)
+    @total_non_creator_users = @non_creator_users.count
+            
+    #cours suivis
+    @workshops = Workshop.all
+    @total_attendances = 0
+    @workshops.each do |workshop|
+      @total_attendances += workshop.attendances.count
+
     @top_categories = Category.left_joins(:workshops).group(:id).order('COUNT(workshops.id) DESC').limit(8)
     @workshops = Workshop.where(validated: true, brouillon: false).to_a.select do |workshop|
       workshop.status == 'en cours' || workshop.status == 'à venir'
+
     end
   end
+end
 end
